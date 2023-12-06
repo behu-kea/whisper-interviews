775baeddb621e6ea86cf5474172effc7ab5fad9d
behu-kea <78361068+behu-kea@users.noreply.github.com>
2023-12-06 11:19:45 +0100

    Update speech-to-text.py

--- speech-to-text-whisper/speech-to-text.py
+++ speech-to-text-whisper/speech-to-text.py
@@ -9,8 +9,7 @@ load_dotenv()
 
 AudioSegment.converter = which("ffmpeg")
 openai.api_key = os.environ.get("OPENAI_KEY")
-# interviewees = [{"name": "allan", "language": 'da'}, {"name": "andreas", "language": 'da'}, {"name": "benjamin", "language": 'da'}, {"name": "marta", "language": 'en'}, {"name": "natalia", "language": 'en'}, {"name": "niels", "language": 'da'}, {"name": "rasmus", "language": 'en'}]
-interviewees = [{"name": "ditlev", "language": 'da'}]
+interviewees = [{"name": "INTERVIEWEE", "language": 'da'}]
 
 for interviewee in interviewees:
     print("Started with " + interviewee['name'])
